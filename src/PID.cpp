7ea467fb64855683db6b0704ed282ced344129dc
KiwiVision <john@kiwicampus.com>
2019-04-05 18:00:29 -0500

    general updates and changes

--- src/PID.cpp
+++ src/PID.cpp
@@ -9,22 +9,39 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp_, double Ki_, double Kd_) {
-  /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   */
+  /*
+  * Initialize PID controller with the coefficients as the input values.
+  */
+  
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
+
+  d_error = 0;
+  p_error = 0;
+  i_error = 0;
 
 }
 
 void PID::UpdateError(double cte) {
   /**
-   * TODO: Update PID errors based on cte.
-   */
+  * Update PID errors based on cte.
+  */
 
+  // d_error is difference from old cte (p_error) to the new cte
+  d_error = (cte - p_error);
+
+  // p_error gets set to the new cte
+  p_error = cte;
+
+  // i_error is the sum of ctes to this point
+  i_error += cte;
+  
 }
 
 double PID::TotalError() {
   /**
-   * TODO: Calculate and return the total error
-   */
-  return 0.0;  // TODO: Add your total error calc here!
+  * Calculate and return the total error
+  */
+  return -Kp * p_error - Kd * d_error - Ki * i_error;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -35,28 +35,36 @@ int main() {
 
   PID pid;
   /**
-   * TODO: Initialize the pid variable.
+   * Initialize the pid variable.
    */
+
+  std::cout << "Starting PID program" << std::endl;
+  pid.Init(0.2, 0.004, 3.0); // CHECK THIS CHECK THISCHECK THISCHECK THISCHECK THISCHECK THISCHECK THISCHECK THIS
+  std::cout << "PID Control initialized" << std::endl;
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
-    if (length && length > 2 && data[0] == '4' && data[1] == '2') {
-      auto s = hasData(string(data).substr(0, length));
 
+    if (length && length > 2 && data[0] == '4' && data[1] == '2') 
+    {
+      auto s = hasData(string(data).substr(0, length));
       if (s != "") {
         auto j = json::parse(s);
-
         string event = j[0].get<string>();
-
         if (event == "telemetry") {
+          
           // j[1] is the data JSON object
           double cte = std::stod(j[1]["cte"].get<string>());
           double speed = std::stod(j[1]["speed"].get<string>());
           double angle = std::stod(j[1]["steering_angle"].get<string>());
           double steer_value;
+          std::cout << "OK" << std::endl;
+         
+
+          //*******************************************************************
           /**
            * TODO: Calculate steering value here, remember the steering value is
            *   [-1, 1].
@@ -64,13 +72,27 @@ int main() {
            *   Maybe use another PID controller to control the speed!
            */
           
+          // Update error values with cte
+          pid.UpdateError(cte);
+
+          // Calculate steering value (if reasonable error, returns between [-1, 1])
+          steer_value = pid.TotalError();
+
+          //*******************************************************************
+
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
                     << std::endl;
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
+
+          // Setting throttle with same PID controller as steering
+          // Formula below switches to between [0, 1], larger steering angle means less throttle
+          // Multiplied by 0.5 for safety reasons
+          msgJson["throttle"] = (1 - std::abs(steer_value)) * 0.5 + 0.2;
+          // msgJson["throttle"] = 0.3;
+
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
@@ -100,6 +122,5 @@ int main() {
     std::cerr << "Failed to listen to port" << std::endl;
     return -1;
   }
-  
   h.run();
 }
